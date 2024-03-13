ce638cb8399fa33faf551a8965fbda947dd9c526
Kebdani Mohammed <kebdaniserjhnx@gmail.com>
2024-03-13 05:44:57 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [active, setActive] = useState(true);
+
+  const [product, setProduct] = useState({
+    name: "",
+    discription: "",
+    price: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/products")
+      .then((response) => {
+        setProducts(response.data.products);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data", error);
+      });
+  }, []);
+
+  const submit = () => {
+    axios
+      .post("http://localhost:3000/add-product", product)
+      .then((response) => {
+        setProducts([...products, response.data.product]);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data", error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      {active && (
+        <div className="model">
+          <div className="up">
+            <h1> Add product </h1>
+            <h1 style={{ cursor: "pointer" }} onClick={(e) => setActive(false)}>
+              X
+            </h1>
+          </div>
+
+          <form>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={product.name}
+              onChange={(e) => setProduct({ ...product, name: e.target.value })}
+            />
+
+            <label htmlFor="price">Price</label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              value={product.price}
+              onChange={(e) =>
+                setProduct({ ...product, price: e.target.value })
+              }
+            />
+
+            <label htmlFor="discription">Discription</label>
+            <textarea
+              id="discription"
+              name="discription"
+              value={product.discription}
+              onChange={(e) =>
+                setProduct({ ...product, discription: e.target.value })
+              }
+            ></textarea>
+
+            <button
+              type="submit"
+              style={{
+                background: "black",
+                color: "white",
+                padding: "5px",
+                cursor: "pointer",
+              }}
+              onClick={e=>submit()}
+            >
+              Add product
+            </button>
+          </form>
+        </div>
+      )}
+
+      <header>
+        <h1>Products</h1>
+        <p style={{ cursor: "pointer" }} onClick={(e) => setActive(true)}>
+          + Add product
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <ul>
+        {products.length > 0 &&
+          products.map((product) => {
+            return (
+              <li key={product.id}>
+                <div className="product">
+                  <div className="up">
+                    <h3> {product.name} </h3>
+                    <p> {product.price}$ </p>
+                  </div>
+                  <p> {product.discription} </p>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 }
